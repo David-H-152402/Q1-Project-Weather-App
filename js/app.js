ff8c6248a462339c936a30c2a1689ca92d4bdd19
David Hernandez <David.Hernandez0118@gmail.com>
2016-09-01 14:54:11 -0600

    Got main weather panel to work

--- js/app.js
+++ js/app.js
@@ -14,15 +14,42 @@ $(document).ready(function() {
       url: 'http://api.wunderground.com/api/683a8f0a30e49337/forecast/q/CO/Boulder.json',
       dataType: 'json',
       success: function(data) {
-          console.log('success!', data);
-          var weatherData = {
+        console.log('success!', data);
+        var weatherData = {};
+        weatherData.today = data.forecast.txt_forecast.forecastday[0].title
+        weatherData.time = data.forecast.txt_forecast.date
+        weatherData.icon = data.forecast.txt_forecast.forecastday[0].icon_url
+        weatherData.conditions = data.forecast.txt_forecast.forecastday[0].fcttext
+        weatherData.hiTempFahrenheit = data.forecast.simpleforecast.forecastday[0].high.fahrenheit
+        console.log(weatherData.hiTempFahrenheit);
+        weatherData.hiTempCelsius = data.forecast.simpleforecast.forecastday[0].high.celsius;
+        console.log(weatherData.hiTempCelsius);
+        weatherData.loTempFahrenheit = data.forecast.simpleforecast.forecastday[0].low.fahrenheit;
+        console.log(weatherData.loTempFahrenheit);
+        weatherData.loTempCelsius = data.forecast.simpleforecast.forecastday[0].low.celsius;
+        console.log(weatherData.loTempCelsius);
+        weatherData.maxWind = data.forecast.simpleforecast.forecastday[0].maxwind.mph + data.forecast.simpleforecast.forecastday[0].maxwind.dir
+        weatherData.test = weatherData.maxWind
+        console.log(weatherData.test);
+        putDataIn(weatherData);
+      }
 
-
-          };
-        }
-        //     error: function(err) {
-        //       console.log('Your Weather was Not Found', err.submit - button);
-        //     }
+      //     error: function(err) {
+      //       console.log('Your Weather was Not Found', err.submit - button);
+      //     }
     })
   };
+
+  function putDataIn(weatherData) {
+    console.log();
+    $('#day').text(weatherData.today);
+    $('#time').text("Taken " + weatherData.time);
+    // $('#icon').text(weatherData.icon);
+    $('#hitemp').text("High : " +
+      weatherData.hiTempFahrenheit + " F " + " | " + weatherData.hiTempCelsius + " C ")
+    $('#lotemp').text("Low : " +
+      weatherData.loTempFahrenheit + " F " + " | " + weatherData.loTempCelsius + " C ")
+    $('#conditions').text(weatherData.conditions);
+    $('#wind').text("Max Wind Speed : " + weatherData.maxWind);
+  }
 });
