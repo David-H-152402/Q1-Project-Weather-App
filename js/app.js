42f9816360db818ee7254d781e573bc6c14d5389
David Hernandez <David.Hernandez0118@gmail.com>
2016-09-02 13:16:01 -0600

    updated layout & CSS

--- js/app.js
+++ js/app.js
@@ -1,63 +1,116 @@
 $(document).ready(function() {
-  console.log("ready!");
   $('#modal1').openModal();
-  var testing;
-
   $('#modal-button').on('click', function(event) {
     event.preventDefault();
-    var myCity = $('#city').val()
-    var myState = $('#state').val()
-    console.log('myCity', myCity);
-    console.log('myState', myState);
+    const myCity = $('#city').val()
+    const myState = $('#state').val()
     ajaxRequest(myCity, myState);
   });
 
-
   function ajaxRequest(city, state) {
-    console.log("ajax request invoked");
     $.ajax({
       method: 'GET',
       url: `http://api.wunderground.com/api/683a8f0a30e49337/forecast/q/${state}/${city}.json`,
       dataType: 'json',
       success: function(data) {
         console.log('success!', data);
-        var weatherData = {};
+        // ================================================================ Day [0]
+        const weatherData = {};
         weatherData.today = data.forecast.txt_forecast.forecastday[0].title
         weatherData.time = data.forecast.txt_forecast.date
         weatherData.icon = data.forecast.txt_forecast.forecastday[0].icon_url
         weatherData.conditions = data.forecast.txt_forecast.forecastday[0].fcttext
         weatherData.hiTempFahrenheit = data.forecast.simpleforecast.forecastday[0].high.fahrenheit
-        console.log(weatherData.hiTempFahrenheit);
         weatherData.hiTempCelsius = data.forecast.simpleforecast.forecastday[0].high.celsius;
-        console.log(weatherData.hiTempCelsius);
         weatherData.loTempFahrenheit = data.forecast.simpleforecast.forecastday[0].low.fahrenheit;
-        console.log(weatherData.loTempFahrenheit);
         weatherData.loTempCelsius = data.forecast.simpleforecast.forecastday[0].low.celsius;
-        console.log(weatherData.loTempCelsius);
-        weatherData.maxWind = data.forecast.simpleforecast.forecastday[0].maxwind.mph + data.forecast.simpleforecast.forecastday[0].maxwind.dir
-        weatherData.test = weatherData.maxWind
-        console.log(weatherData.test);
-
+        weatherData.maxWind = data.forecast.simpleforecast.forecastday[0].maxwind.dir + " " + data.forecast.simpleforecast.forecastday[0].maxwind.mph + " " + "mph"
         putDataIn(weatherData);
+        //================================================================ day[1]
+        var weatherData1 = {};
+        weatherData1.today = data.forecast.txt_forecast.forecastday[2].title
+        weatherData1.time = data.forecast.txt_forecast.date
+        weatherData1.icon = data.forecast.txt_forecast.forecastday[2].icon_url
+        weatherData1.conditions = data.forecast.txt_forecast.forecastday[2].fcttext
+        weatherData1.hiTempFahrenheit = data.forecast.simpleforecast.forecastday[1].high.fahrenheit
+        weatherData1.hiTempCelsius = data.forecast.simpleforecast.forecastday[1].high.celsius;
+        weatherData1.loTempFahrenheit = data.forecast.simpleforecast.forecastday[1].low.fahrenheit;
+        weatherData1.loTempCelsius = data.forecast.simpleforecast.forecastday[1].low.celsius;
+        weatherData1.maxWind = data.forecast.simpleforecast.forecastday[1].maxwind.dir + " " + data.forecast.simpleforecast.forecastday[1].maxwind.mph + " " + "mph"
+        putDataIn1(weatherData1);
+        //================================================================ day[2]
+        var weatherData2 = {};
+        weatherData2.today = data.forecast.txt_forecast.forecastday[4].title
+        weatherData2.time = data.forecast.txt_forecast.date
+        weatherData2.icon = data.forecast.txt_forecast.forecastday[4].icon_url
+        weatherData2.conditions = data.forecast.txt_forecast.forecastday[4].fcttext
+        weatherData2.hiTempFahrenheit = data.forecast.simpleforecast.forecastday[2].high.fahrenheit
+        weatherData2.hiTempCelsius = data.forecast.simpleforecast.forecastday[2].high.celsius;
+        weatherData2.loTempFahrenheit = data.forecast.simpleforecast.forecastday[2].low.fahrenheit;
+        weatherData2.loTempCelsius = data.forecast.simpleforecast.forecastday[2].low.celsius;
+        weatherData2.maxWind = data.forecast.simpleforecast.forecastday[2].maxwind.dir + " " + data.forecast.simpleforecast.forecastday[2].maxwind.mph + " " + "mph"
+        putDataIn2(weatherData2);
+        //================================================================ Monday[3]
+        var weatherData3 = {};
+        weatherData3.today = data.forecast.txt_forecast.forecastday[6].title
+        weatherData3.time = data.forecast.txt_forecast.date
+        weatherData3.icon = data.forecast.txt_forecast.forecastday[6].icon_url
+        weatherData3.conditions = data.forecast.txt_forecast.forecastday[6].fcttext
+        weatherData3.hiTempFahrenheit = data.forecast.simpleforecast.forecastday[3].high.fahrenheit
+        weatherData3.hiTempCelsius = data.forecast.simpleforecast.forecastday[3].high.celsius;
+        weatherData3.loTempFahrenheit = data.forecast.simpleforecast.forecastday[3].low.fahrenheit;
+        weatherData3.loTempCelsius = data.forecast.simpleforecast.forecastday[3].low.celsius;
+        weatherData3.maxWind = data.forecast.simpleforecast.forecastday[3].maxwind.dir + " " + data.forecast.simpleforecast.forecastday[3].maxwind.mph + " " + "mph"
+        putDataIn3(weatherData3);
       }
-
-      //     error: function(err) {
-      //       console.log('Your Weather was Not Found', err.submit - button);
-      //     }
     })
   };
-
+  //=============================================================== Day[0]
   function putDataIn(weatherData) {
-    console.log();
-    $('#day').text(weatherData.today);
-    $('#time').text("Taken " + weatherData.time);
-    // $('#icon').text(weatherData.icon);
-    $('#hitemp').text("High : " +
-      weatherData.hiTempFahrenheit + " F " + " | " + weatherData.hiTempCelsius + " C ")
-    $('#lotemp').text("Low : " +
-      weatherData.loTempFahrenheit + " F " + " | " + weatherData.loTempCelsius + " C ")
-    $('#conditions').text(weatherData.conditions);
-    $('#wind').text("Max Wind Speed : " + weatherData.maxWind);
-    $('#icon').html(`<img src="${weatherData.icon}">`)
+    $('#day0').text(weatherData.today);
+    $('#time0').text("Taken " + weatherData.time);
+    $('#icon0').html(`<img src="${weatherData.icon}">`);
+    $('#hitemp0').text("High : " +
+      weatherData.hiTempFahrenheit + " F " + " | " + weatherData.hiTempCelsius + " C ");
+    $('#lotemp0').text("Low : " +
+      weatherData.loTempFahrenheit + " F " + " | " + weatherData.loTempCelsius + " C ");
+    $('#conditions0').text(weatherData.conditions);
+    $('#wind0').text("Wind Speed : " + weatherData.maxWind);
+  }
+  //============================================================= day[1]
+  function putDataIn1(weatherData1) {
+    $('#day1').text(weatherData1.today);
+    $('#time1').text("Taken " + weatherData1.time);
+    $('#icon1').html(`<img src="${weatherData1.icon}">`);
+    $('#hitemp1').text("High : " +
+      weatherData1.hiTempFahrenheit + " F " + " | " + weatherData1.hiTempCelsius + " C ");
+    $('#lotemp1').text("Low : " +
+      weatherData1.loTempFahrenheit + " F " + " | " + weatherData1.loTempCelsius + " C ");
+    $('#conditions1').text(weatherData1.conditions);
+    $('#wind1').text("Wind Speed : " + weatherData1.maxWind);
+  }
+  //============================================================ day[2]
+  function putDataIn2(weatherData2) {
+    $('#day2').text(weatherData2.today);
+    $('#time2').text("Taken " + weatherData2.time);
+    $('#icon2').html(`<img src="${weatherData2.icon}">`);
+    $('#hitemp2').text("High : " +
+      weatherData2.hiTempFahrenheit + " F " + " | " + weatherData2.hiTempCelsius + " C ");
+    $('#lotemp2').text("Low : " +
+      weatherData2.loTempFahrenheit + " F " + " | " + weatherData2.loTempCelsius + " C ");
+    $('#conditions2').text(weatherData2.conditions);
+    $('#wind2').text("Wind Speed : " + weatherData2.maxWind);
+  }
+  //============================================================ day[3]
+  function putDataIn3(weatherData3) {
+    $('#day3').text(weatherData3.today);
+    $('#time3').text("Taken " + weatherData3.time);
+    $('#icon3').html(`<img src="${weatherData3.icon}">`);
+    $('#hitemp3').text("High : " +
+      weatherData3.hiTempFahrenheit + " F " + " | " + weatherData3.hiTempCelsius + " C ");
+    $('#lotemp3').text("Low : " +
+      weatherData3.loTempFahrenheit + " F " + " | " + weatherData3.loTempCelsius + " C ");
+    $('#conditions3').text(weatherData3.conditions);
+    $('#wind3').text("Wind Speed : " + weatherData3.maxWind);
   }
 });
